e7e46b51919c5aebf41eb3b7cb83a7fed96cc02d
hoangtrung225 <hoangtrung225@gmail.com>
2017-05-20 08:27:24 -0700

    finish fork redirect and exec

--- simple-telnet-server/telnetd.c
+++ simple-telnet-server/telnetd.c
@@ -5,10 +5,15 @@
 #include <unistd.h>
 #include <string.h>
 #include <netdb.h>
-
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #define DEFAULT_PORT "2323"
 #define BACKLOG 10
 #define BUFFSIZE 1024 //* 64
+
+int send_shell_header(int sock, char* buffer);
+
 int main(int argc, char** argv){
     struct addrinfo hints, *res;
     struct sockaddr_storage client_addr;
@@ -38,7 +43,7 @@ int main(int argc, char** argv){
 	struct sockaddr_in *s = (struct sockaddr_in *)&client_addr;
 	char ipstr[14];
 	int port;
-	printf("Connect from socket: %d", new_fd);
+	printf("Connect from socket: %d\n", new_fd);
 
 	if(new_fd < 0){
 		printf("Error: server fail to establis connection\n");
@@ -47,23 +52,57 @@ int main(int argc, char** argv){
 	else{
 	    port = ntohs(s->sin_port);
 	    inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-		printf("Accept connection from %s:%d:", ipstr, port);
+		printf("Accept connection from %s:%d\n", ipstr, port);
 	}
 	char buffer[BUFFSIZE];
-	while(1){
+		while(1){
 		byte = send_shell_header(new_fd, buffer);
 		if(byte < 0){
 			printf("Error: connection lost!\n");
 			close(new_fd);
 			return 0;
 		}
+
 		byte = recv(new_fd, buffer, BUFFSIZE, 0);
 		if(byte < 0){
 			printf("Error: connection lost!\n");
 			close(new_fd);
 			return 0;
 		}
-		byte = send(new_fd, buffer, BUFFSIZE, 0);
+
+		buffer[byte - 1] = '\0';
+	
+		int pid;
+		pid = fork();
+		
+		//parrent stuff
+		if(pid > 0){
+			waitpid(pid, NULL, 0);
+			continue;
+		}
+		//child process
+		if(pid == 0){
+			//redirect STOUT STDERR to new_fd
+			dup2(new_fd, STDOUT_FILENO);
+ 			dup2(new_fd, STDERR_FILENO);
+
+			//close server listening socket
+			close(sockfd);
+		
+			//parse user send message
+			char* path;
+			char* argv[16];
+			path = strtok(buffer, " ");
+			argv[0] = path;
+			for(int i = 0; i < 16; i++){
+				argv[i+1] = strtok(NULL, " ");
+				if(argv[i+1] == NULL) break;
+			}
+			argv[15] = NULL;
+		 
+			execvp(path, argv);
+			exit(0);
+		}
 	}
 }
 
@@ -84,7 +123,8 @@ int send_shell_header(int sock, char* buffer){
 		send(sock, buffer, BUFFSIZE, 0);
 		return 0;
 	}
-	int buff_len = strlen(buffer);
+	int buff_len = 0;
+	buff_len = strlen(buffer);
 	buffer[buff_len] = '@';
 	if(getcwd(buffer + buff_len + 1, BUFFSIZE - buff_len - 1) == NULL){
 		printf("Error: system failure\n");
@@ -92,6 +132,7 @@ int send_shell_header(int sock, char* buffer){
 		send(sock, buffer, BUFFSIZE, 0);
 		return 0;
 	}
-	int i = send(sock, buffer, strlen(buffer), 0);
+	sprintf(buffer + strlen(buffer), " :\n");
+	int i = send(sock, buffer, strlen(buffer) + 1, 0);
 	return i;
 }
